9e6daf0ed8850c952469696cf3fb51e853b9c355
shwetapund <pundshweta2020@gmail.com>
2024-03-17 05:23:07 -0700

    create folder structure and routing

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import Home from "./views/Home";
+import About from "./views/About";
+import Dashboard from "./views/Dashboard";
+import Login from "./views/Login";
+import Signup from "./views/SignUp";
+import Projects from "./views/Projects";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Home />
+    },
+    {
+        path:'/about',
+        element:<About />
+    },
+    {
+        path:'/login',
+        element:<Login />
+    },
+    {
+        path:'/signup',
+        element:<Signup />
+    },
+    {
+        path:'/projects',
+        element:<Projects />
+    },
+    {
+        path:'/dashboard',
+        element:<Dashboard/>
+    }
+])
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+  <RouterProvider router={router}/>
+  </>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
