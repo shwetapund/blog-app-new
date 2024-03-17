124d817ec8b1b78fb84caadcba7c27604ea4e32b
shwetapund <pundshweta2020@gmail.com>
2024-03-17 05:35:10 -0700

    changes in routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "./views/Home";
+import About from "./views/About";
+import Dashboard from "./views/Dashboard";
+import Login from "./views/Login";
+import Signup from "./views/SignUp";
+import Projects from "./views/Projects";
 
 function App() {
   return (
     <>
-     <h1 className='text-3xl text-red-500'>app</h1>
+     {/* <h1 className='text-3xl text-red-500'>app</h1> */}
+     <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/projects' element={<Projects />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login />}/>
+
+     </Routes>
+     </BrowserRouter>
     </>
    
   )
--- client/src/index.js
+++ client/src/index.js
@@ -2,45 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {RouterProvider, createBrowserRouter} from 'react-router-dom';
-import Home from "./views/Home";
-import About from "./views/About";
-import Dashboard from "./views/Dashboard";
-import Login from "./views/Login";
-import Signup from "./views/SignUp";
-import Projects from "./views/Projects";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<Home />
-    },
-    {
-        path:'/about',
-        element:<About />
-    },
-    {
-        path:'/login',
-        element:<Login />
-    },
-    {
-        path:'/signup',
-        element:<Signup />
-    },
-    {
-        path:'/projects',
-        element:<Projects />
-    },
-    {
-        path:'/dashboard',
-        element:<Dashboard/>
-    }
-])
 root.render(
   <>
-  <RouterProvider router={router}/>
+  <App/>
   </>
 );
 
