9087f2fba6dfd69029ff32235270f34b3a60cca0
shwetapund <pundshweta2020@gmail.com>
2024-03-18 01:51:35 -0700

    create a database connection

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+const connectMongoDB = async ()=>{
+    const conn = await mongoose.connect(process.env.MONGODB_URI)
+    if(conn){
+        console.log('💖mongoDB is connected');
+    }
+}
+connectMongoDB();
+
 
 app.listen(PORT, ()=>{
     console.log(`port is running on ${PORT}`)
